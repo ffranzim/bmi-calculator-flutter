25cbba73e9c6fd785c50a796ac03231a2c1db9f0
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-07-17 22:54:01 -0300

    Secao 12 - Aula 132 - Fazendo 'esqueleto' do layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,8 +14,60 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(colour: Color(0XFFD1E33)),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(colour: Color(0XFFD1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: ReusableCard(colour: Color(0XFFD1E33)),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(colour: Color(0XFFD1E33)),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ReusableCard(colour: Color(0XFFD1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+
+  ReusableCard({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: this.colour,
       ),
     );
   }
